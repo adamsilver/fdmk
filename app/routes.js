e7cb3f448f3065b0a14a0eba409a970a6a7733f4
Adam Silver <adambsilver@gmail.com>
2024-01-17 14:43:25 +0000

    Add new screens

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,16 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+const flash = require('connect-flash')
+router.use(flash())
+
+router.all('*', (req, res, next) => {
+  res.locals.flash = req.flash('success')
+  next()
+})
+
 // Add your routes here
+router.post('/email-preferences', (req, res) => {
+  req.flash('success', 'Email settings saved')
+  res.redirect(`/email-preferences`)
+})
