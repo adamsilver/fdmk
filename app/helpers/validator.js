091b595c8a8d90e652adb1aec86650e859f24208
Adam Silver <adambsilver@gmail.com>
2025-01-17 09:27:56 +0000

    Make validator work for date inputs so that the red border appears around the problematic inputs

--- app/helpers/validator.js
+++ app/helpers/validator.js
@@ -1,97 +1,102 @@
 const _ = require('lodash');
 
-function Validator(req, res) {
-  this.req = req;
-  this.res = res;
-	this.validators = [];
-	this.errors = [];
-}
+class Validator {
+  constructor(req, res) {
+    this.req = req;
+    this.res = res;
+    this.validators = [];
+    this.errors = [];
+    this.highlights = {};
+  }
 
-Validator.prototype.add = function(params) {
-	this.validators.push(params);
-};
+  add(params) {
+    this.validators.push(params);
+  }
 
-Validator.prototype.getErrors = function() {
-	return this.errors;
-};
+  getErrors() {
+    return this.errors;
+  }
 
-Validator.prototype.getFormattedErrors = function() {
-	return this.errors.map(this.formatError);
-};
+  getFormattedErrors() {
+    return this.errors.map(this.formatError);
+  }
 
-Validator.prototype.getInlineErrors = function() {
-  let inlineErrors = {}
-  this.errors.forEach(error => {
-    const { name, message } = error;
-    _.set(inlineErrors, name, { text: message })
-  });
-  return inlineErrors
-};
+  getInlineErrors() {
+    const inlineErrors = {};
+    this.errors.forEach(error => {
+      const { name, message } = error;
+      _.set(inlineErrors, name, { text: message });
+    });
+    return inlineErrors;
+  }
 
-Validator.prototype.getErrorSummary = function() {
-  return { items: this.getFormattedErrors() }
-};
+  getErrorSummary() {
+    return { items: this.getFormattedErrors() };
+  }
 
-Validator.prototype.getErrorHighlights = function() {
-  return this.highlights
-};
+  getErrorHighlights() {
+    return this.highlights;
+  }
 
-Validator.prototype.formatError = function(error) {
-	return {
-    text: error.message,
-		href: '#' + error.name
-	};
-};
+  formatError(error) {
+    return {
+      text: error.message,
+      href: `#${error.name}`
+    };
+  }
 
-Validator.prototype.validate = function() {
-  this.generateErrors()
-  this.generateHighlights()
-  this.errors.length === 0
-}
+  validate() {
+    this.generateErrors();
+    this.generateHighlights();
+    return this.errors.length === 0;
+  }
 
-// For error messages (show 1 per field)
-Validator.prototype.generateErrors = function() {
-  this.errors = []
+  generateErrors() {
+    this.errors = [];
 
-  for( let validator of this.validators ) {
-    const { name, rules } = validator;
-    const value = _.get(this.req.body, name)
+    for (let validator of this.validators) {
+      const { name, rules } = validator;
+      const value = _.get(this.req.body, name);
 
-    for( let rule of rules ){
-      const { fn, params, message } = rule;
-      const validatorReturnValue = fn( value, params );
+      for (let rule of rules) {
+        const { fn, params, message } = rule;
+        const validatorReturnValue = fn(value, params);
 
-      if(typeof validatorReturnValue === 'boolean' && !validatorReturnValue ){
-        this.errors.push( { name, message } );
-        break;
-      } else if(typeof validatorReturnValue == 'string') {
-        this.errors.push( { name: validatorReturnValue, message } );
-        break;
+        // If the rule returns a false boolean, the name is the name of the field
+        if (typeof validatorReturnValue === 'boolean' && !validatorReturnValue) {
+          this.errors.push({ name, message });
+          break;
+        // If the rule returns a string, the name is that string
+        } else if (typeof validatorReturnValue === 'string') {
+          this.errors.push({ name: validatorReturnValue, message });
+          break;
+        }
+      }
+    }
+  }
+
+  generateHighlights() {
+    this.highlights = {};
+
+    for (let validator of this.validators) {
+      const { name, rules } = validator;
+      const value = _.get(this.req.body, name);
+
+      for (let rule of rules) {
+        const { fn, params } = rule;
+        const validatorReturnValue = fn(value, params);
+
+        // If the rule returns a false boolean, the name is the name of the field
+        if (typeof validatorReturnValue === 'boolean' && !validatorReturnValue) {
+          _.set(this.highlights, name, {});
+
+        // If the rule returns a string, the name is that string
+        } else if (typeof validatorReturnValue === 'string') {
+          _.set(this.highlights, validatorReturnValue, {});
+        }
       }
     }
   }
 }
-
-// For highlighting inputs with a red border (show all per field - for date inputs basically)
-Validator.prototype.generateHighlights = function() {
-  this.highlights = {}
-
-  for( let validator of this.validators ) {
-    const { name, rules } = validator;
-    const value = _.get(this.req.body, name)
-
-    for( let rule of rules ){
-      const { fn, params } = rule;
-      const validatorReturnValue = fn( value, params );
-
-      if(typeof validatorReturnValue === 'boolean' && !validatorReturnValue ){
-        _.set(this.highlights, name, {})
-      } else if(typeof validatorReturnValue == 'string') {
-        _.set(this.highlights, validatorReturnValue, {})
-      }
-    }
-  }
-}
-
 
 module.exports = Validator;
